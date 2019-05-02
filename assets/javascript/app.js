7f0ae08c933c7980b9f0d1ba0648676fc0d0c230
cclivick <46388110+cclivick@users.noreply.github.com>
2019-05-02 16:20:56 -0500

    Third Commit - JS Questions, Choices, CorrectAnswers, and Images added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,43 +1,81 @@
 //Create var to hold questions
-var questionBank = [
-    "What are the Coen Brothers first names?",
-    "What was the Coen Brothers first film?",
-    "In which Coen Brothers film does a car salesman stage his wife's kidnapping?",
-    "In which Coen Brothers film does a Vietnam veteran become the target of a hitman after stealing $2 million of drug money?",
-    "In which Coen Brothers film is a bowling-obsessed slacker mistaken for a millionaire that shares his name?",
-    "In Barton Fink (1991), John Goodman's character reveals himself to be a famous serial killer named what?",
-    "In Burn After Reading (2008), Brad Pitt's character believes that he stumbles on top secret government intelligence. What does it turn out that he is actually reading?",
-    "O Brother, Where Art Thou? (2000) is the Coen Brothers Depression-era take on which famous story?",
-    "What is the Coen Brothers top grossing film?",
-    "Which of the following major Hollywood actors/actresses has never appeared in a Coen Brothers film?"
-]
 
-var correctAnswer;
-var userGuess;
+$(document).ready(function() { 
+
+//Create global variables for gameplay    
+    var questionBank = [{
+        
+        question: "What are the Coen Brothers first names?",
+        choices: ["James and Evan", "Joel and Ethan", "Jeff and Eric", "Jacob and Eldyn"],
+        correctAnswer: 1,
+        image: ("assets/images/Coenbrothers.jpg")
+        }, {
+        question: "What was the Coen Brothers first film?",
+        choices:["Barton Fink", "Raising Arizona", "Blood Simple", "Miller's Crossing"],
+        correctAnswer: 2,
+        image: ("assets/images/bloodsimple.jpg")
+        }, {
+        question:"In which Coen Brothers film does a car salesman stage his wife's kidnapping?",
+        choices:["Fargo", "A Serious Man", "Intolerable Cruelty", "The Ladykillers"],
+        correctAnswer: 0,
+        image: ("assets/images/fargo.jpg")
+        }, {
+        question:"In which Coen Brothers film does a Vietnam veteran become the target of a hitman after stealing $2 million of drug money?",
+        choices:["The Ballad of Buster Scruggs", "The Hudsucker Proxy", "Suburbicon", "No Country for Old Men"],
+        correctAnswer: 3,
+        image: ("assets/images/nocountry.jpg") 
+        }, {
+        question: "In which Coen Brothers film is a bowling-obsessed slacker mistaken for a millionaire that shares his name?",
+        choices:["True Grit", "The Big Lebowski", "Crimewave", "The Man Who Wasn't There"],
+        correctAnswer: 1,
+        image: ("assets/images/lebowski.jpg")
+        }, {
+        question:"In Barton Fink (1991), John Goodman's character reveals himself to be a famous serial killer named what?",
+        choices:["Mad Dog Marzen", "Mac the Monster", "Maniac Miller", "Madman Mundt"],
+        correctAnswer: 3,
+        image: ("assets/images/barton.jpg")
+        }, {
+        question:"In Burn After Reading (2008), Brad Pitt's character believes that he stumbles on top secret government intelligence. What does it turn out that he is actually reading?",
+        choices:["[John Malkovich]'s memoirs", "[George Clooney]'s phone records", "[Frances McDormand]'s expense reports", "[JK Simmons] javascript code"],
+        correctAnswer: 0,
+        image: ("assets/images/burnafter.gif")
+        }, {
+        question: "O Brother, Where Art Thou? (2000) is the Coen Brothers Depression-era take on which famous story?",
+        choices: ["The Iliad", "The Odyssey", "Heracles (Twelve Labors)", "Myth of Sisyphus"],
+        correctAnswer: 1,
+        image: ("assets/images/obrother.jpg")
+        }, {
+        question: "What is the Coen Brothers top grossing film?",
+        choices: ["The Big Lebowski", "No Country for Old Men", "True Grit", "Fargo"],
+        correctAnswer: 2,
+        image: ("assets/images/truegrit.jpg")
+        }, {
+        question: "Which of the following major Hollywood actors/actresses has never appeared in a Coen Brothers film?",
+        choices: ["William H Macy", "John Turturro", "Christian Bale", "Paul Newman"],
+        correctAnswer: 2,
+        image: ("assets/images/bale.jpg")
+        }
+    ]
 
 
 
-//Create global variables for gameplay
-var clockRunning = false;
-var timer = 30;
 
-//Click event that removes all content from container div
-$('button').on("click", function() {
-    console.log("button clicked");
-    $('.gameTitle').empty().removeClass('gameTitle').addClass('question');
-    $('.startButton').remove();
-//Populates .question div with first question and 4 possible answers
-    $('.question').text(questionBank[0]);
-    $('.container').append('<button type=button class=answer choice1>James and Evan</button>');
-    $('.container').append('<button type=button class=answer choice2>Joel and Ethan</button>');
-    $('.container').append('<button type=button class=answer choice3>Jeff and Eric</button>');
-    $('.container').append('<button type=button class=answer choice4>Jacob and Eldyn</button>');
 
-    clockRunning = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
-//If user clicks correct answer, create content in container div that congratulates them and shows a picture for 4 seconds
-//If user clicks wrong answer, create content in container div that tells them the correct answer and shows a picture for 4 seconds
-//If timer runs out, create content in container div that tells them they ran out of time and shows a picture for 4 seconds
-//After 4 seconds, go to next question and restart timer
-//After all questions displayed, show number of correct answers and incorrect answers
-//Create option to restart without reloading the page
